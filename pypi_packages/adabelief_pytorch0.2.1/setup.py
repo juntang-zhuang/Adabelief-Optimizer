b625fbf5cbf8bcd85396666485fd47086d2e0024
juntang-zhuang <j.zhuang@yale.edu>
2021-02-14 15:13:50 -0500

    update_0.2.1

---
+++ pypi_packages/adabelief_pytorch0.2.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="adabelief_pytorch", 
+    version="0.9.1",
+    author="Juntang Zhuang",
+    author_email="j.zhuang@yale.edu",
+    description="PyTorch implementation of AdaBelief Optimizer",
+    long_description="PyTorch implementation of AdaBelief Optimizer",
+    long_description_content_type="text/markdown",
+    url="https://juntang-zhuang.github.io/adabelief/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'torch>=0.4.0',
+          'colorama>=0.4.0',
+          'tabulate>=0.7',
+      ],
+    python_requires='>=3.6',
+)
